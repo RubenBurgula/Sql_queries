db48bb24198eb88df163a9b820b4bf7816e4e923
Ruben <rubenburgula@gmail.com>
2025-06-16 14:17:45 -0500

    Delete helllo.sql

--- helllo.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tranasctions(amount)
-VALUES (222),
-		(112);
-        
-SELECT * FROM transactions;
-
-alter table transactions
-modify COLUMN trans_ID INT PRIMARY KEY AUTO_INCREMENT=1001;
