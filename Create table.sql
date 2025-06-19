19ceba9ef62cfd8ebac762cdf061421da6d1932a
Ruben <rubenburgula@gmail.com>
2025-06-19 18:57:49 -0500

    Add files via upload

---
+++ Create table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Place (
+    place_id INT PRIMARY KEY,
+    place_name VARCHAR(100),
+    place_type VARCHAR(50),
+    city VARCHAR(100),
+    state VARCHAR(50),
+    zip_code VARCHAR(10)
+);
