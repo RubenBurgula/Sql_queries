e0e78d51fee85b811d77dcd54f4e2216251ab43f
Ruben <rubenburgula@gmail.com>
2025-06-19 19:25:44 -0500

    Add files via upload

---
+++ INSERT Multiple Rows.sql
@@ -0,0 +1,4 @@
+INSERT INTO location (location_id, location_name, location_type, city, state, zip_code)
+VALUES 
+(2, 'Downtown Hub', 'Station', 'Austin', 'TX', '73301'),
+(3, 'North Center', 'Office', 'Houston', 'TX', '77001');
